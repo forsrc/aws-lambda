108d821d33fc5152f36816097b5056639d42b4f4
forsrc <forsrc@gmail.com>
2018-02-23 21:28:29 +0900

    Create EsHttpResponseHandler.java

---
+++ es/src/main/java/com/forsrc/aws/es/EsHttpResponseHandler.java
@@ -0,0 +1,43 @@
+package com.forsrc.aws.es;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+import com.amazonaws.AmazonWebServiceResponse;
+import com.amazonaws.http.HttpResponse;
+import com.amazonaws.http.HttpResponseHandler;
+import com.amazonaws.util.IOUtils;
+import com.amazonaws.util.json.Jackson;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+
+public class EsHttpResponseHandler implements HttpResponseHandler<AmazonWebServiceResponse<Map<String, Object>>> {
+
+	private CompletableFuture<Map<String, Object>> completableFuture;
+
+	public EsHttpResponseHandler(CompletableFuture<Map<String, Object>> completableFuture) {
+		this.completableFuture = completableFuture;
+	}
+
+	@Override
+	public AmazonWebServiceResponse<Map<String, Object>> handle(HttpResponse response) throws Exception {
+		String text = IOUtils.toString(response.getContent());
+		TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {
+		};
+		Map<String, Object> map = new ObjectMapper().readValue(text, typeRef);
+		System.out.println("---> count: " + map.get("conut"));
+		System.out.println("--> EsHttpResponseHandler: " + response.getStatusCode());
+		System.out.println("--> EsHttpResponseHandler: " + text);
+		AmazonWebServiceResponse<Map<String, Object>> awsResponse = new AmazonWebServiceResponse<>();
+		awsResponse.setResult(map);
+		completableFuture.complete(map);
+		return awsResponse;
+	}
+
+	@Override
+	public boolean needsConnectionLeftOpen() {
+		return false;
+	}
+}
